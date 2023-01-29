6ede4935292243ad92df0af9cc13ff1fca2b6a6d
Protract-123 <vermayuvraj2010@gmail.com>
2023-01-29 22:09:00 +0530

    Added a custom currency system along with a custom save data system

---
+++ Currency/CustomCurrency.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GameConsole;
+using RogueKill.Utils;
+
+namespace RogueKill.Currency
+{
+    public static class CustomCurrency
+    {
+        public static int baseAmount = 100;
+        public static int levelsCompleted = 0;
+        public static int currentAmount = 0;
+
+        public static void AddCurrency(bool bossLevel)
+        {
+            if(CustomSaveData.ReadFromSave("credit") != null)
+            {
+                currentAmount = int.Parse(CustomSaveData.ReadFromSave("credit"));  
+            }
+            
+            StatsManager stats = MonoSingleton<StatsManager>.Instance;
+            int kills = stats.kills;
+
+            if (currentAmount <= 0)
+            {
+                currentAmount = 0;
+            }
+            
+            int addedAmount = baseAmount + (levelsCompleted * 25) + (kills);
+            
+            if (bossLevel) {
+                addedAmount += 100;
+            }
+            
+            currentAmount += addedAmount;
+            CustomSaveData.WriteToSave("credit", currentAmount.ToString());
+        }
+        public static void RemoveCurrency(int amount) {
+            
+            if (CustomSaveData.ReadFromSave("credit") != null)
+            {
+                currentAmount = int.Parse(CustomSaveData.ReadFromSave("credit"));
+            }
+            
+            currentAmount -= amount;
+            CustomSaveData.WriteToSave("credit", currentAmount.ToString());
+        }
+    }
+}
---
+++ Utils/CustomSaveData.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace RogueKill.Utils
+{
+    public static class CustomSaveData
+    {
+        public static string path = $"{Application.persistentDataPath}\\achievements.rkuc";
+
+
+        public static void WriteToSave(string key, string value)
+        {
+            if(!File.Exists(path)) { 
+                File.Create(path);
+            }
+            string[] lines = File.ReadAllLines(path);
+            int currentLine = 0;
+            foreach(string line in lines)
+            {
+                if (line.Contains(key))
+                {
+                    lines[currentLine] = $"{key}:{value}";
+                    File.WriteAllLines(path, lines);
+                    return;
+                }
+                currentLine++;
+            }
+            lines.AddItem<string>($"{key}:{value}");
+            File.WriteAllLines(path, lines);
+        }
+        public static string ReadFromSave(string key)
+        {
+            if(!File.Exists(path))
+            {
+                File.Create(path);
+                return null;
+            }
+            string[] lines = File.ReadAllLines(path);
+            foreach(string line in lines) { 
+                if(line.Contains(key))
+                {
+                    string[] values = line.Split(':');
+                    return values[1];
+                }
+            }
+            return null;
+        }
+    }
+}
